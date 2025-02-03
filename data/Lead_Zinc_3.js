22aee1f7763524d0cc6bd97a7b552a8870c3ed54
Sameer A. Butala <sameerabutala@gmail.com>
2025-02-03 10:48:48 +0530

    Add files via upload

---
+++ data/Lead_Zinc_3.js
@@ -0,0 +1,1 @@
+var json_Lead_Zinc_3 = {"type":"FeatureCollection","name":"Lead_Zinc_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","Name":null,"Name-1":"Lead & Zinc"},"geometry":{"type":"Point","coordinates":[72.034576457976655,25.890157173929964]}}]}
